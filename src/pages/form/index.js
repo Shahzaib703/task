d8171c2b81030e1feef119f30f4cf6b31c61e0f5
unknown <mirzashahzaib703@gmail.com>
2022-02-08 18:58:18 +0500

    first commit

---
+++ src/pages/form/index.js
@@ -0,0 +1,107 @@
+import Container from "components/container";
+import Input from "components/input";
+import Select from "components/select";
+import React from "react";
+import { useState } from "react";
+import style from "./form.module.scss";
+
+const Form = () => {
+  const [name, setname] = useState("");
+
+  const [email, setemail] = useState("");
+
+  const [gender, setgender] = useState("");
+
+  const [phone, setphone] = useState("");
+
+  const submit = (e) => {
+
+    if(name===""){
+      alert("Name should not be empty")
+      return;
+    }
+    if(name.length <=3){
+      alert("Name should be at least 3 characters long")
+      return;
+    }
+
+    if(email===""){
+      alert("The email field should not be empty")
+      return
+    }
+    
+    if(phone.length<10){
+      alert("The phoneNumber sholud be valid Pakistani phone number")
+      return  
+    }
+    
+    fetch("https://react-coding-challenge-dev.herokuapp.com/api/users", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        phone: phone,
+        gender: gender,
+      }),
+    });
+  };
+
+
+function set(e){
+  alert("y")
+}
+
+  return (
+    <Container>
+      <form onSubmit={submit}>
+        <div className={style.grid}>
+          <Input
+            label="Name"
+            error={true}
+            errorMessage={"This field is required"}
+            name="name"
+            type="text"
+            placeholder="name"
+            onChange={(e) => {
+              setname(e.target.value);
+            }}
+          />
+          <Input
+            label="Email"
+            name="email"
+            type="email"
+            placeholder="email"
+            onChange={(e) => {
+              setemail(e.target.value);
+            }}
+          />
+          <Select
+            label="Gender"
+            placeHolder="Gender"
+            onChange={e => setgender(e.target.value)}
+            options={options}
+          />
+          <Input
+            name="phone"
+            type="number"
+            label="Phone"
+            placeholder="3007689786"
+            onChange={(e) => {
+              setphone(e.target.value);
+            }}
+          />
+        </div>
+        <div className={style.button}>
+          <button>Submit</button>
+        </div>
+      </form>
+    </Container>
+  );
+};
+export default Form;
+
+const options = ["Male", "Female"];
